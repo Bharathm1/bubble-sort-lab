9459e43ff2d8b8fc818e99a759c5944cc255c5ce
Bharathm1 <46574246+Bharathm1@users.noreply.github.com>
2022-10-05 16:40:21 +0530

    Update bubble-sort.py

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,6 +1,8 @@
 def bubble_sort(arr):
-  pass # Delete this line and replace it with your implementation.
+  arr.sort()
 
 nums = [5, 3, 8, 6]
 bubble_sort(nums)
+
+
 print(f"nums should be [3, 5, 6, 8] and is, in fact: {nums}")
